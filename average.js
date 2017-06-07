c078669547538fe9b7b4a8cf35264cf32c7a1bf5
Jonas Perusquia <jonaspm_99@outlook.com>
2017-06-07 13:43:31 -0600

    Catch error if sum/cells.length equals 0

--- average.js
+++ average.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-    $('#content-title')
-        .append(`<strong title="Presióname" id="average">Promedio: Presiona Aquí</strong>`)
-    $('#average')
-        .css('float', 'right')
-        .click(getAverage)
+  $('#content-title')
+    .append(`<strong title="Presióname" id="average">Promedio: Presiona Aquí</strong>`)
+  $('#average')
+    .css('float', 'right')
+    .click(getAverage)
 })
 
 function getAverage(e) {
-    e.preventDefault()
-    let sum = 0,
-        average = 0,
-        cells = $('tr.trKardexGris td:nth-child(4)')
-    try {
-        cells.each(function() {
-            sum += Number.parseFloat($(this).text())
-        })
-    } catch (ex) {
-        alert('Error: No se pudo calcular el promedio')
-        console.log(ex);
-        return;
-    }
+  e.preventDefault()
+  let sum = 0,
+      average = 0,
+      cells = $('tr.trKardexGris td:nth-child(4)')
 
+  try {
+    cells.each(function() {
+        sum += Number.parseFloat($(this).text())
+    })
     average = sum / cells.length
-    $('#average').text(`Promedio: ${average.toFixed(2)}`)
+  } catch (ex) {
+    alert('Error: No se pudo calcular el promedio')
+    console.log(ex);
+    return;
+  }
+  $('#average').text(`Promedio: ${average.toFixed(2)}`)
 }
